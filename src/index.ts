908d6333abf6870bf060cdafd00b3a50c8b48b58
Arie Miller <renari@arimil.com>
2020-01-05 11:13:27 -0500

    clean up some code and support http://anidb.net/a14564 link format

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import anidb from './anidb';
 import logger from './logger';
 
 export default class SaberAlter {
+  private readonly anidbClient: anidb;
+  private readonly discordClient: Discord.Client;
+
   public static readonly log = logger({
     timestamp: 'mm/dd/yy HH:MM:ss',
     debug: process.env.NODE_ENV !== 'production',
@@ -19,40 +22,42 @@ export default class SaberAlter {
   }
 
   constructor() {
-    const client = new Discord.Client();
-    const anidbClient = new anidb();
+    this.discordClient = new Discord.Client();
+    this.anidbClient = new anidb();
 
-    client.once('ready', () => {
+    this.discordClient.once('ready', () => {
       SaberAlter.log.info('discord connection successful');
-      client.on('message', message => {
-        const match = message.content.match(/https?:\/\/(?:www\.)?anidb\.net\/anime\/(\d+)/);
-        if (match) {
-          anidbClient
-            .getShowData(match[1])
-            .then(data => {
-              const embed = new Discord.RichEmbed();
-              embed.setTitle(data.anime.titles[0].title[0]._);
-              embed.setAuthor(
-                data.anime.url[0],
-                'https://cdn.anidb.net/css/icons/touch/favicon-32x32.png',
-                data.anime.url[0],
-              );
-              embed.setDescription(SaberAlter.formatDiscordMessage(data.anime.description[0]));
-              embed.setThumbnail('https://cdn.anidb.net/images/main/' + data.anime.picture);
-              embed.setURL(match[0]);
-              embed.addField('Episodes', data.anime.episodecount[0]);
-              return message.channel.send(embed);
-            })
-            .catch(err => {
-              SaberAlter.log.error(err);
-            });
-        }
-      });
+      this.discordClient.on('message', this.messageHandler);
     });
 
-    client.login(process.env.DISCORD_TOKEN).catch(err => {
+    this.discordClient.login(process.env.DISCORD_TOKEN).catch(err => {
       SaberAlter.log.error(err);
     });
+  }
+
+  private messageHandler(message: Discord.Message): void {
+    const match = message.content.match(/https?:\/\/(?:www\.)?anidb\.net\/a(?:nime\/)?(\d+)/gm);
+    if (match) {
+      this.anidbClient
+        .getShowData(match[1])
+        .then(data => {
+          const embed = new Discord.RichEmbed();
+          embed.setTitle(data.anime.titles[0].title[0]._);
+          embed.setAuthor(
+            data.anime.url[0],
+            'https://cdn.anidb.net/css/icons/touch/favicon-32x32.png',
+            data.anime.url[0],
+          );
+          embed.setDescription(SaberAlter.formatDiscordMessage(data.anime.description[0]));
+          embed.setThumbnail('https://cdn.anidb.net/images/main/' + data.anime.picture);
+          embed.setURL(match[0]);
+          embed.addField('Episodes', data.anime.episodecount[0]);
+          return message.channel.send(embed);
+        })
+        .catch(err => {
+          SaberAlter.log.error(err);
+        });
+    }
   }
 }
 
