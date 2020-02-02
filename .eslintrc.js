4bca27400d2607441f2baf18110b06a1334a42e1
Arie Miller <renari@arimil.com>
2020-02-02 05:39:20 -0500

    make eslint stricter with console calls

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'error',
+    'no-debugger': 'error',
     '@typescript-eslint/class-name-casing': 'off',
   },
   parserOptions: {
